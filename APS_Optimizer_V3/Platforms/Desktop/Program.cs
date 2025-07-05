e9bee419144eaa11affc66348caf35e8a4845e8d
trk20 <trkennedy01@gmail.com>
2025-07-05 15:48:25 -0400

    before I mess everything up

--- APS_Optimizer_V3/Platforms/Desktop/Program.cs
+++ APS_Optimizer_V3/Platforms/Desktop/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Uno.UI.Runtime.Skia;
 
 namespace APS_Optimizer_V3;
@@ -7,14 +8,25 @@ public class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        var host = SkiaHostBuilder.Create()
-            .App(() => new App())
-            .UseX11()
-            .UseLinuxFrameBuffer()
-            .UseMacOS()
-            .UseWindows()
-            .Build();
+        try
+        {
+            AppDomain.CurrentDomain.SetData("APP_CONTEXT_BASE_DIRECTORY",
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
 
-        host.Run();
+            var host = SkiaHostBuilder.Create()
+                .App(() => new App())
+                .UseX11()
+                .UseLinuxFrameBuffer()
+                .UseMacOS()
+                .UseWindows()
+                .Build();
+
+            host.Run();
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("error.log", ex.ToString());
+            throw;
+        }
     }
 }
